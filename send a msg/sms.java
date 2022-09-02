4ad2552c52385b3b76f539a3ba80c06bae1ef3c3
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2022-09-02 16:20:08 +0900

    Create sms.java

---
+++ send a msg/sms.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import org.json.simple.JSONObject;
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+public class sms {
+  public static void main(String[] args) {
+    String api_key = "API key";  //https://coolsms.co.kr/ 에서 키 발급 받기
+    String api_secret = "API secret";
+    Message coolsms = new Message(api_key, api_secret);
+
+    // 4 params(to, from, type, text) are mandatory. must be filled
+    HashMap<String, String> params = new HashMap<String, String>();
+    params.put("to", "01012341234");	// 수신전화번호
+    params.put("from", "01011111111");	// 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
+    params.put("type", "LMS");  // 긴 문자 형식
+    params.put("text", "저는 문자전송 관련 코딩을 하는 중입니다. 그래서...이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고 지금은 당신에게로 옮겨진 이 편지는 4일 안에 당신 곁을 떠나야 합니다. 이 편지를 포함해서 7통을 행운이 필요한 사람에게 보내 주셔야 합니다. 복사를 해도 좋습니다. 혹 미신이라 하실지 모르지만 사실입니다.\r\n"
+    		+ "\r\n"
+    		+ "영국에서 HGXWCH이라는 사람은 1930년에 이 편지를 받았습니다. 그는 비서에게 복사해서 보내라고 했습니다. 며칠 뒤에 복권이 당첨되어 20억을 받았습니다. 어떤 이는 이 편지를 받았으나 96시간 이내 자신의 손에서 떠나야 한다는 사실을 잊었습니다. 그는 곧 사직되었습니다. 나중에야 이 사실을 알고 7통의 편지를 보냈는데 다시 좋은 직장을 얻었습니다. 미국의 케네디 대통령은 이 편지를 받았지만 그냥 버렸습니다. 결국 9일 후 그는 암살당했습니다. 기억해 주세요. 이 편지를 보내면 7년의 행운이 있을 것이고 그렇지 않으면 3년의 불행이 있을 것입니다. 그리고 이 편지를 버리거나 낙서를 해서는 절대로 안됩니다. 7통입니다. 이 편지를 받은 사람은 행운이 깃들것입니다. 힘들겠지만 좋은게 좋다고 생각하세요. 7년의 행운을 빌면서...편지를 보내세요.");
+    params.put("app_version", "test app 1.2"); // application name and version
+
+    try {
+      JSONObject obj = (JSONObject) coolsms.send(params);
+      System.out.println(obj.toString());
+    } catch (CoolsmsException e) {
+      System.out.println(e.getMessage());
+      System.out.println(e.getCode());
+    }
+  }
+}
